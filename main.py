b177348f3a52015bed7786adf4d27f37abdd5c14
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2023-07-04 20:16:36 -0600

    Added the create profile function

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ if not os.path.exists(folderPath):
     print("Created 'assets' folder")
 else:
     print("'assets' folder already exists")
+
+#%% Create a profile
+def createProfile(profileName):
+    folderName = os.path.join(os.getcwd(), "assets/" + str(profileName))
+    
+    if not os.path.exists(folderName):
+        os.makedirs(folderName)
+        print("Created '" + str(profileName) + "' profile")
+    else:
+        print("'" + str(profileName) + "' profile already exists")
